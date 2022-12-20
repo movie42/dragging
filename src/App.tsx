ab4364f996a47a9d52ecd72470b13acdc03ed4ef
movie42 <movie3342@gmail.com>
2022-12-20 11:26:34 +0900

    feat: 드레깅 구현
    onMouseDown,
    - 옮기고자 하는 컴포넌트의 마우스 버튼을 눌렀을 때 isDragging을 true로 변경합니다.
    onMouseUp
    - 옮기고자 하는 컴포넌트의 마우스 버튼을 뗄 때, isDragging을 false로 변경합니다.
    onMouseMove
    - 드래그 영역 안에서 마우스 포인터의 현재 위치를 offset 상태에 저장합니다.
    onBoxMove
    - 드래그하고자 하는 컴포넌트를 드래그 가능한 영역 안에서 위치를 계산하여 boxOffset 상태에 저장합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Box, Draggable } from "./Components";
+
+interface Offset {
+  x: null | number;
+  y: null | number;
+}
 
 function App() {
+  const [isDragging, setIsDragging] = useState(false);
+  const [offset, setOffset] = useState<Offset>({ x: null, y: null });
+  const [boxOffset, setBoxOffset] = useState<Offset>({ x: null, y: null });
+  const onMouseDown = () => {
+    setIsDragging(true);
+  };
+  const onMouseUp = () => {
+    setIsDragging(false);
+  };
+  const onMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDragging) {
+      return;
+    }
+    const target = e.currentTarget;
+    let offsetX = e.clientX - target.getBoundingClientRect().left;
+    let offsetY = e.clientY - target.getBoundingClientRect().top;
+
+    setOffset({ x: offsetX, y: offsetY });
+  };
+  const onBoxMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDragging) {
+      return;
+    }
+
+    if (offset.x && offset.y) {
+      let offsetX = offset.x - e.currentTarget.clientWidth / 2;
+      let offsetY = offset.y - e.currentTarget.clientHeight / 2;
+      setBoxOffset({ x: offsetX, y: offsetY });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Draggable onMouseMove={onMouseMove}>
+        <Box
+          onMouseDown={onMouseDown}
+          onMouseUp={onMouseUp}
+          onMouseMove={onBoxMove}
+          style={{
+            transform: `translate3d(${boxOffset.x}px, ${boxOffset.y}px, 0)`
+          }}
+        />
+      </Draggable>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
